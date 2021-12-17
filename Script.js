0245f421ac67616cc387ff4288e94a256554064d
Anurag021 <anu.rawat@outlook.com>
2021-12-17 12:56:01 +0530

    Update Code
    Update Code

---
+++ Script.js
@@ -0,0 +1,21 @@
+const Stocks =[{
+Company_Name: "DEEPAKNITRATE", Category : "Chemical" , Average_Price:"2247.75", Current_Price:"2324.95",Lowest_Cut_Off:"2150", Highest_Cut_Off:"2500"},
+{Company_Name: "DMART", Category : "Cosumer Retail", Average_Price:"4542.1", Current_Price:"4814.4",Lowest_Cut_Off:"4600", Highest_Cut_Off:"5000"},
+{Company_Name: "HCLTECH", Category : "IT Services", Average_Price:"1229.73", Current_Price:"1229.73",Lowest_Cut_Off:"1100", Highest_Cut_Off:"1300"}] 
+
+{/* <tr style="background-color:green" >
+<td>1</td>
+<td>DEEPAKNTR</td>
+<td>Chemical</td> 
+<td>2247.75</td>
+<td>2324.95</td>  
+<td>2150</td> 
+<td>2500</td>
+</tr> */}
+
+function generateTable(){
+
+
+
+
+}
